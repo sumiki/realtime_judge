fa27f713a5c14043a1a6b770e337bfd07e149fa8
Sumiki Ohsawa <sumikio@gmail.com>
2017-11-04 00:14:08 -0700

    firebase install

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: 'AIzaSyCWMeMFRZrbvv8QqsYghwPaf50WDxoqjPY',
+  authDomain: 'hello-e207d.firebaseapp.com',
+  databaseURL: 'https://hello-e207d.firebaseio.com',
+  projectId: 'hello-e207d',
+  storageBucket: '',
+  messagingSenderId: "492607833085"
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
