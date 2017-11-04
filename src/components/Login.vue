67f0c258ca8be233a3e0e6e3cdd13b8d40a1e0fc
Sumiki Ohsawa <sumikio@gmail.com>
2017-11-03 23:52:34 -0700

    signup vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,4 +19,25 @@ export default {
 </script>
 
 <style scoped>  /* "scoped" attribute limit the CSS to this component only */
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
 </style>
