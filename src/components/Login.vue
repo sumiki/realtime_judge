c4bc2949e27daa3899e17799a435b1889d87f69d
Sumiki Ohsawa <sumikio@gmail.com>
2017-11-03 23:46:26 -0700

    login component

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button>Connection</button>
+    <p>You don't have an account</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>  /* "scoped" attribute limit the CSS to this component only */
+</style>
