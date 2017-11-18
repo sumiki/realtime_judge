4461859922331a4922627c03d8ec989eccd080c3
Sumiki Ohsawa <sumikio@gmail.com>
2017-11-17 21:11:46 -0800

    home update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,55 @@
 <template>
-<div>HOME</div>
+<div class="container">
+  <div class="page-header">
+    <h1>Realtime Judge</h1>
+  </div>
+  <div class="panel-heading">
+    <h3>Heats</h3>
+  </div>
+  <div class="panel-body">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Heat Name</th>
+            <th>Start Time</th>
+            <th class="player">Player</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="heat in heats">
+            <td>{{ heat.name }}</td>
+            <td>{{ heat.start_time }}</td>
+            <td>
+            <div v-for="player in heat.players">
+              <span class="color_tag" v-bind:class="[ `${player.color}_tag` ]" >{{ player.color }}</span>
+              <span>{{ player.name }}</span>
+            </div>
+            </td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</div>
+
 </template>
 
 <script>
+import firebase from '../firebase'
+import './../assets/color_tags.css'
+let db = firebase.database();
+let heatsRef = db.ref('heats');
 export default {
   name: 'Home',
+  firebase: function(){
+    return {
+      heats: heatsRef.orderByChild('published').equalTo( true )
+    }
+  },
   created: function(){
-  console.log('aa')
   }
-  }
+}
 </script>
 
 <style scoped>
--- src/components/admin.vue
+++ src/components/admin.vue
@@ -77,7 +77,7 @@ import firebase from '../firebase'
 import './../assets/color_tags.css'
 let db = firebase.database();
 let heatsRef = db.ref('heats');
-
+let pointsRef = db.ref('points');
 
 export default {
   name: 'Admin',
